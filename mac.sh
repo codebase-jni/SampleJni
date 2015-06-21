018b051ab55ed65426cf508ec89940a433277673
aromal <aromalsasidharan@gmail.com>
2015-06-21 23:08:42 +0530

    command in git changed

--- mac.sh
+++ mac.sh
@@ -1,11 +1,13 @@
 
 echo "The command was $1"
 
-if [ "$1" == "run" ]; then
+if [ "$1" == "clean" ];
+then
+	rm  -rf ./libJniSample.dylib ./JniSample.h ./JniSample.class
+else
+	rm  -rf ./libJniSample.dylib ./JniSample.h ./JniSample.class
 	javac JniSample.java
 	javah JniSample
 	gcc -I/Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX10.10.sdk/System/Library/Frameworks/JavaVM.framework/Versions/A/Headers -o libJniSample.dylib -shared JniSample.c
-	java JniSample	
-else
-	rm  -rf ./libJniSample.dylib ./JniSample.h ./JniSample.class
+	java JniSample
 fi
